5fa5895261241035290dec54b9c31b7dd25924c6
wildenis medeiros <wildenismedeiros@hotmail.com>
2017-01-13 03:14:14 -0200

    .

--- IdentificandoIdade/IdentificandoIdade/Form1.Designer.cs
+++ IdentificandoIdade/IdentificandoIdade/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace IdentificandoIdade
 {
-    partial class Form1
+    partial class FormIdentificandoIdade
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblDataNascimento = new System.Windows.Forms.Label();
+            this.dtpNascimento = new System.Windows.Forms.DateTimePicker();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.btnIdentificarIdade = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(12, 24);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(49, 17);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "&Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(67, 23);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(269, 21);
+            this.txtNome.TabIndex = 1;
+            // 
+            // lblDataNascimento
+            // 
+            this.lblDataNascimento.AutoSize = true;
+            this.lblDataNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataNascimento.Location = new System.Drawing.Point(12, 65);
+            this.lblDataNascimento.Name = "lblDataNascimento";
+            this.lblDataNascimento.Size = new System.Drawing.Size(140, 17);
+            this.lblDataNascimento.TabIndex = 2;
+            this.lblDataNascimento.Text = "&Data de Nascimento:";
+            // 
+            // dtpNascimento
+            // 
+            this.dtpNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNascimento.Location = new System.Drawing.Point(158, 65);
+            this.dtpNascimento.Name = "dtpNascimento";
+            this.dtpNascimento.Size = new System.Drawing.Size(91, 20);
+            this.dtpNascimento.TabIndex = 3;
+            this.dtpNascimento.ValueChanged += new System.EventHandler(this.dtpNascimento_ValueChanged);
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdade.Location = new System.Drawing.Point(199, 128);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(126, 17);
+            this.lblIdade.TabIndex = 4;
+            this.lblIdade.Text = "IDADE DO ALUNO";
+            this.lblIdade.Visible = false;
+            // 
+            // btnIdentificarIdade
+            // 
+            this.btnIdentificarIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIdentificarIdade.Location = new System.Drawing.Point(15, 125);
+            this.btnIdentificarIdade.Name = "btnIdentificarIdade";
+            this.btnIdentificarIdade.Size = new System.Drawing.Size(118, 23);
+            this.btnIdentificarIdade.TabIndex = 5;
+            this.btnIdentificarIdade.Text = "Identificar Idade";
+            this.btnIdentificarIdade.UseVisualStyleBackColor = true;
+            this.btnIdentificarIdade.Click += new System.EventHandler(this.btnIdentificarIdade_Click);
+            // 
+            // FormIdentificandoIdade
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(395, 178);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.btnIdentificarIdade);
+            this.Controls.Add(this.lblIdade);
+            this.Controls.Add(this.dtpNascimento);
+            this.Controls.Add(this.lblDataNascimento);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Name = "FormIdentificandoIdade";
+            this.Text = "Matrícula de aluno";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblDataNascimento;
+        private System.Windows.Forms.DateTimePicker dtpNascimento;
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.Button btnIdentificarIdade;
     }
 }
 
--- IdentificandoIdade/IdentificandoIdade/Form1.cs
+++ IdentificandoIdade/IdentificandoIdade/Form1.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace IdentificandoIdade
 {
-    public partial class Form1 : Form
+    public partial class FormIdentificandoIdade : Form
     {
-        public Form1()
+        public FormIdentificandoIdade()
         {
             InitializeComponent();
+        }
+
+        private void btnIdentificarIdade_Click(object sender, EventArgs e)
+        {
+            int Idade;
+            Idade = DateTime.Now.Year - dtpNascimento.Value.Year;
+            MessageBox.Show(Idade.ToString());
+            lblIdade.Visible = true;
+            lblIdade.Text = "" + Idade.ToString();
+
+        }
+
+        private void dtpNascimento_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
