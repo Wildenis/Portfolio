0a05bc15938ff9c9989edc886966d952bcce0195
wildenis medeiros <wildenismedeiros@hotmail.com>
2017-01-16 14:52:03 -0200

    .

---
+++ CalcPesoIdeal/CalcPesoIdeal/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace CalcPesoIdeal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpSexo = new System.Windows.Forms.GroupBox();
+            this.rbnMasculino = new System.Windows.Forms.RadioButton();
+            this.rbnFeminino = new System.Windows.Forms.RadioButton();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblPesoIdeal = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.grpSexo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpSexo
+            // 
+            this.grpSexo.Controls.Add(this.rbnFeminino);
+            this.grpSexo.Controls.Add(this.rbnMasculino);
+            this.grpSexo.Location = new System.Drawing.Point(23, 25);
+            this.grpSexo.Name = "grpSexo";
+            this.grpSexo.Size = new System.Drawing.Size(159, 48);
+            this.grpSexo.TabIndex = 0;
+            this.grpSexo.TabStop = false;
+            this.grpSexo.Text = "Sexo";
+            // 
+            // rbnMasculino
+            // 
+            this.rbnMasculino.AutoSize = true;
+            this.rbnMasculino.Location = new System.Drawing.Point(6, 19);
+            this.rbnMasculino.Name = "rbnMasculino";
+            this.rbnMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rbnMasculino.TabIndex = 0;
+            this.rbnMasculino.TabStop = true;
+            this.rbnMasculino.Text = "&Masculino";
+            this.rbnMasculino.UseVisualStyleBackColor = true;
+            this.rbnMasculino.CheckedChanged += new System.EventHandler(this.rbnMasculino_CheckedChanged);
+            // 
+            // rbnFeminino
+            // 
+            this.rbnFeminino.AutoSize = true;
+            this.rbnFeminino.Location = new System.Drawing.Point(85, 19);
+            this.rbnFeminino.Name = "rbnFeminino";
+            this.rbnFeminino.Size = new System.Drawing.Size(67, 17);
+            this.rbnFeminino.TabIndex = 1;
+            this.rbnFeminino.TabStop = true;
+            this.rbnFeminino.Text = "&Feminino";
+            this.rbnFeminino.UseVisualStyleBackColor = true;
+            this.rbnFeminino.CheckedChanged += new System.EventHandler(this.rdbFeminino_CheckedChanged);
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(221, 25);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(37, 13);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "Altura:";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(224, 41);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 2;
+            this.txtAltura.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtAltura.TextChanged += new System.EventHandler(this.txtAltura_TextChanged);
+            // 
+            // lblPesoIdeal
+            // 
+            this.lblPesoIdeal.AutoSize = true;
+            this.lblPesoIdeal.Location = new System.Drawing.Point(150, 90);
+            this.lblPesoIdeal.Name = "lblPesoIdeal";
+            this.lblPesoIdeal.Size = new System.Drawing.Size(60, 13);
+            this.lblPesoIdeal.TabIndex = 3;
+            this.lblPesoIdeal.Text = "Peso Ideal:";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.ForeColor = System.Drawing.Color.Red;
+            this.lblPeso.Location = new System.Drawing.Point(172, 128);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(13, 20);
+            this.lblPeso.TabIndex = 4;
+            this.lblPeso.Text = ".";
+            this.lblPeso.Click += new System.EventHandler(this.lblPeso_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 186);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblPesoIdeal);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.grpSexo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grpSexo.ResumeLayout(false);
+            this.grpSexo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpSexo;
+        private System.Windows.Forms.RadioButton rbnMasculino;
+        private System.Windows.Forms.RadioButton rbnFeminino;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblPesoIdeal;
+        private System.Windows.Forms.Label lblPeso;
+    }
+}
+
